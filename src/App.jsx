1dad0e80be1ebea4699113706fc353896793ee67
jonathanj101 <jonatjimen@gmail.com>
2024-11-22 19:42:34 -0500

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    {/* <Route
+            path="/"
+            element={<LandingPage/>}
+          >
+
+          </Route> */}
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Layout.jsx
@@ -0,0 +1,6 @@
+/* 
+This file is created to ensure the default layout of the app  and having the Header and Footer display acroos all pages 
+
+*/
+
+//  TODO

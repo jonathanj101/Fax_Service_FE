43ae492d0df99ab8ab654f09b67cf6984680ca06
jonathanj101 <jonatjimen@gmail.com>
2024-12-05 23:59:24 -0500

    Footer created and styled

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./footerStyles.css";
+
+const Footer = () => {
+    return (
+        <div id="footer">
+            {/* <!-- Footer --> */}
+            <footer>
+                <p>&copy; 2024 FaxService. All rights reserved.</p>
+                <nav>
+                    <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+                </nav>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
